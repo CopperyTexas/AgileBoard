56a2786bb314035fb960f9cec2d993255e726db8
Denis Glebov <dengleboff@yandex.ru>
2025-06-11 10:39:18 +0500

    добавил зависимости

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="font-semibold text-2xl">Vite + React</h1>
       <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
